b0850bf716e9ecd71b76a2089a3b9f009ad92264
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2022-12-13 16:25:33 +0800

    setup express & mongodb connections

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import express from "express";
+// import logger from "morgan";
+import dotenv from "dotenv";
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+import indexRouter from "./routes/index.js";
+import mongoose from "mongoose";
 
 var app = express();
+const env = dotenv.config().parsed;
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
-module.exports = app;
+app.listen(env.APP_PORT, () => {
+	console.log(`Server is running on port ${env.APP_PORT}`);
+});
+
+// connect Mongodb
+mongoose.set("strictQuery", true);
+mongoose.connect(`${env.MONGODB_URI}${env.MONGODB_HOST}:${env.MONGODB_PORT}`, {
+	dbName: env.MONGODB_DB_NAME,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+	console.log("Connected to MongoDB");
+});
