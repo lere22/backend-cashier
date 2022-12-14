e63df177fdde2a08b6062a20b8fc436b73dc6ee8
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2022-12-14 16:09:46 +0800

    setup categories (model, controller, view)

---
+++ controllers/CategoryController.js
@@ -0,0 +1,61 @@
+import category from "../models/Category.js";
+
+const index = async (req, res) => {
+	try {
+		const categories = await category.find();
+
+		if (!categories) {
+			throw {
+				code: 500,
+				message: "Get all item in categories failed",
+			};
+		}
+
+		return res.status(200).json({
+			status: true,
+			total: categories.length,
+			categories,
+		});
+	} catch (err) {
+		return res.status(err.code).json({
+			status: false,
+			message: err.message,
+		});
+	}
+};
+
+const store = async (req, res) => {
+	try {
+		if (!req.body.title) {
+			throw {
+				code: 428,
+				message: "Title is required!",
+			};
+		}
+		const title = req.body.title;
+
+		const newCategory = new category({
+			title: title,
+		});
+		const saveCategory = await newCategory.save();
+
+		if (!saveCategory) {
+			throw {
+				code: 500,
+				message: "Store category failed",
+			};
+		}
+
+		return res.status(200).json({
+			status: true,
+			saveCategory,
+		});
+	} catch (err) {
+		return res.status(err.code).json({
+			status: false,
+			message: err.message,
+		});
+	}
+};
+
+export { index, store };
