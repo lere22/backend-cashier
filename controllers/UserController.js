bfb6b9a8645edc0b9f8a6489ddb212d14f507653
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2022-12-30 10:13:18 +0800

    paginate data users with mongoose-paginate-v2

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,8 +2,31 @@ import user from "../models/User.js";
 
 const index = async (req, res) => {
 	try {
-		console.log("Hello");
+		const users = await user.paginate(
+			{},
+			{
+				page: req.query.page || 1,
+				limit: req.query.limit || 10,
+			},
+		);
+
+		// jika data user tidak ada
+		if (!users) {
+			throw {
+				code: 500,
+				message: "Get all item in users failed",
+			};
+		}
+
+		return res.status(200).json({
+			status: true,
+			total: users.length,
+			users,
+		});
 	} catch (err) {
+		if (!err.code) {
+			err.code = 500;
+		}
 		return res.status(err.code).json({
 			status: false,
 			message: err.message,
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
 
 const Schema = mongoose.Schema(
 	{
@@ -32,5 +33,7 @@ const Schema = mongoose.Schema(
 		timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
 	},
 );
+
+Schema.plugin(mongoosePaginate);
 
 export default mongoose.model("User", Schema);
