3f0add9ee7f5a7f3d7e82c528b1830c99bd7ef8e
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2023-01-17 11:45:10 +0800

    middleware role check

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { index, store } from "../controllers/CategoryController.js";
 import jwtAuth from "../middlewares/jwtAuth.js";
+import role from "../middlewares/role.js";
 var router = express.Router();
 
 /* GET categories listing. */
-router.get("/", jwtAuth(), index);
+router.get("/", [jwtAuth(), role(["admin", "cashier"])], index);
 router.post("/", jwtAuth(), store);
 
 export default router;
