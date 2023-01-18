aa4f2e9bc8546915f33436a6e9260f8d71763ca3
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2023-01-18 13:10:06 +0800

    set verify token & role on product

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { index, store } from "../controllers/ProductController.js";
+import jwtAuth from "../middlewares/jwtAuth.js";
+import role from "../middlewares/role.js";
 var router = express.Router();
 
 /* GET products listing. */
-router.get("/", index);
+router.get("/", [jwtAuth(), role(["admin", "cashier"])], index);
 router.post("/", store);
 
 export default router;
